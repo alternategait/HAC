cd33d19fbad9f61fe3ec8a8f94c550630265e864
Brandi <brandihollee@gmail.com>
2022-12-05 17:00:24 -0500

    attempting shopping js and improved signup page

---
+++ public/js/collection.js
@@ -0,0 +1,49 @@
+window.location(/hac/collection)=doShowAll();
+
+// to check browser support though may be redundant
+function checkBrowser(){
+    if ('sessionStorage' in window && window ['sessionStorage'] !== null){
+        return true;
+    } else { return false;}
+}
+
+function doShowAll(){
+    if (checkBrowser()){
+        let key = "";
+        let list = "<tr><th>Item</th><th>Value</th></tr>\n";
+        var i = 0;
+        for (i = 0; i<= sessionStorage.length-1; i++){
+            key = sessionStorage.key(i);
+            list += "<tr><td>" + key + "</td>\n<td>"
+                + sessionStorage.getItem(key) + "</td></tr>\n";
+        }
+        //If no item exists in the cart.
+        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+        }        //Bind the data to HTML table.
+        //You can use jQuery, too.
+        document.getElementById('list').innerHTML = list;
+    } else{
+        alert("Cannot save activity to collection as your browser does not support HTML 5 and session storage")
+    }
+}
+
+function saveItem() {
+
+    var name = actid;
+    var data = actid;
+    sessionStorage.setItem(name, data);
+
+}
+
+function RemoveItem()
+{
+var name=document.forms.shoppingList.name.value;
+document.forms.shoppingList.data.value=sessionStorage.removeItem(name);
+doShowAll();
+}
+
+function ClearAll() {
+    sessionStorage.clear();
+    doShowAll();
+}
