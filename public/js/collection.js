1b63787dc663ae367b13178dcb4a4e6bb8467641
Brandi <brandihollee@gmail.com>
2022-12-27 11:29:04 -0500

    actid added to session storage

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,4 +1,5 @@
-window.location(/hac/collection)=doShowAll();
+// document.getElementById("myBtn").addEventListener("click", displayDate);
+document.getElementById("saveMe").addEventListener("click", saveItem);
 
 // to check browser support though may be redundant
 function checkBrowser(){
@@ -9,30 +10,30 @@ function checkBrowser(){
 
 function doShowAll(){
     if (checkBrowser()){
-        let key = "";
-        let list = "<tr><th>Item</th><th>Value</th></tr>\n";
-        var i = 0;
-        for (i = 0; i<= sessionStorage.length-1; i++){
-            key = sessionStorage.key(i);
-            list += "<tr><td>" + key + "</td>\n<td>"
-                + sessionStorage.getItem(key) + "</td></tr>\n";
-        }
-        //If no item exists in the cart.
-        if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
-            list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-        }        //Bind the data to HTML table.
-        //You can use jQuery, too.
-        document.getElementById('list').innerHTML = list;
+    //     let key = "";
+    //     let list = "<tr><th>Item</th><th>Value</th></tr>\n";
+    //     var i = 0;
+    //     for (i = 0; i<= sessionStorage.length-1; i++){
+    //         key = sessionStorage.key(i);
+    //         list += "<tr><td>" + key + "</td>\n<td>"
+    //             + sessionStorage.getItem(key) + "</td></tr>\n";
+    //     }
+    //     //If no item exists in the cart.
+    //     if (list == "<tr><th>Item</th><th>Value</th></tr>\n") {
+    //         list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
+    //     }        //Bind the data to HTML table.
+    //     //You can use jQuery, too.
+    //     document.getElementById('list').innerHTML = list;
+            console.log("success");
     } else{
         alert("Cannot save activity to collection as your browser does not support HTML 5 and session storage")
     }
 }
 
 function saveItem() {
-
-    var name = actid;
-    var data = actid;
-    sessionStorage.setItem(name, data);
+    var key = document.forms.activity.name.value;
+    var value = document.forms.activity.data.value;
+    sessionStorage.setItem(key, value);
 
 }
 
