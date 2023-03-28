4e1181f8222a3967f1afd62a6e334734ad0f7458
Brandi <brandihollee@gmail.com>
2023-03-28 14:23:22 -0400

    fixed route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const logger = require("morgan");
 const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
-const hacRoutes = require("./routes/hac");
+const hacRoutes = require("./routes/HAC");
 
 
 //Use .env file in config folder
