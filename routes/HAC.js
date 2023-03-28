7cf5615ec6a4014fe758582bccae8278c1eca300
Brandi <brandihollee@gmail.com>
2023-03-28 14:34:25 -0400

    fixed route

--- routes/HAC.js
+++ routes/HAC.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multer");
-const hacController = require("../controllers/hac");
+const hacController = require("../controllers/HAC");
 const homeController = require("../controllers/home");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
